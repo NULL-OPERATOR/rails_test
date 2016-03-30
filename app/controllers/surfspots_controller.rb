16830c070f6e18386826b9a575e3fc08021061a3
NULL-OPERATOR <x@cryptolab.net>
2016-03-30 23:39:28 +0100

    add add surfspot form

--- app/controllers/surfspots_controller.rb
+++ app/controllers/surfspots_controller.rb
@@ -2,4 +2,12 @@ class SurfspotsController < ApplicationController
   def index
     @surfspots = Surfspot.all
   end
+
+  def new
+  end
+
+  def create
+    Surfspot.create(params[:surfspot])
+    redirect_to '/surfspots'
+  end
 end
